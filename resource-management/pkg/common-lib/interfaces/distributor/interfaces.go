cacf2f3680c0cd218ea0a74785a8265cb9742190
Ying Huang <sindica2000@yahoo.com>
2022-05-20 10:19:11 -0700

    Move distributor interface to common-lib (#13)

--- resource-management/pkg/common-lib/interfaces/distributor/interfaces.go
+++ resource-management/pkg/common-lib/interfaces/distributor/interfaces.go
@@ -1,14 +1,11 @@
-package interfaces
+package distributor
 
 import (
 	"global-resource-service/resource-management/pkg/common-lib/types"
 	"global-resource-service/resource-management/pkg/common-lib/types/event"
 )
 
-// TODO:
-//     Coordinate with Distributor with the interfaces defined here.
-//     Distributor will need to make a minor code changes to switch to the new place
-type InterfacesOfDistributor interface {
+type Interface interface {
 	RegisterClient(requestedHostNum int) (string, bool, error)
 	ListNodesForClient(clientId string) ([]*types.Node, types.ResourceVersionMap, error)
 	Watch(clientId string, rvs types.ResourceVersionMap, watchChan chan *event.NodeEvent, stopCh chan struct{}) error
